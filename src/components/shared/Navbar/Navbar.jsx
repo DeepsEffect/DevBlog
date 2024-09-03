8da3f1970c937928e8133f44dd7fb34049f40049
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-03 21:03:14 +0600

    navbar update: added conditional content rendering based on user, avatar dropdown

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -11,11 +11,18 @@ import {
   Link,
   Button,
   Input,
+  Avatar,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown,
 } from "@nextui-org/react";
 import { SearchIcon } from "./SearchIcon";
+import { TfiWrite } from "react-icons/tfi";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const user = true;
 
   return (
     <Nav isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
@@ -27,15 +34,20 @@ export const Navbar = () => {
       {/* title for small view */}
       <NavbarContent className="sm:hidden pr-3" justify="center">
         <NavbarBrand>
-          <p className="font-bold text-inherit">DevBlog</p>
+          <Link href="/" className="font-bold text-inherit">
+            DevBlog
+          </Link>
         </NavbarBrand>
       </NavbarContent>
 
       {/* navbar center content */}
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarBrand>
-          <p className="font-bold text-inherit">DevBlog</p>
+          <Link href="/" className="font-bold text-inherit">
+            DevBlog
+          </Link>
         </NavbarBrand>
+        {/* search bar */}
         <NavbarItem>
           <Input
             classNames={{
@@ -53,19 +65,60 @@ export const Navbar = () => {
         </NavbarItem>
       </NavbarContent>
 
-      {/* show this if no user found */}
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link className="text-text text-sm" href="#">
-            Sign In
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Get Started
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+      {/* conditional rendering  */}
+      {user ? (
+        <>
+          {/* show this if user is available*/}
+          <NavbarContent justify="end">
+            <NavbarItem className="flex justify-center items-center gap-1 cursor-pointer">
+              <TfiWrite />
+              write
+            </NavbarItem>
+            <NavbarItem>
+              <Dropdown className="bg-background" placement="bottom-start">
+                <DropdownTrigger>
+                  <Avatar
+                    isBordered
+                    as="button"
+                    className="transition-transform"
+                    color="secondary"
+                    name="Jason Hughes"
+                    size="sm"
+                    src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                  />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Profile Actions" variant="flat">
+                  <DropdownItem key="profile" className="h-14 gap-2">
+                    <p className="font-semibold">Signed in as</p>
+                    <p className="font-semibold">zoey@example.com</p>
+                  </DropdownItem>
+                  <DropdownItem key="profile">My Profile</DropdownItem>
+                  <DropdownItem key="blogs">My Blogs</DropdownItem>
+                  <DropdownItem key="logout" color="danger">
+                    Log Out
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </NavbarItem>
+          </NavbarContent>
+        </>
+      ) : (
+        <>
+          {/* show this if no user is not available */}
+          <NavbarContent justify="end">
+            <NavbarItem className="hidden lg:flex">
+              <Link className="text-text text-sm" href="#">
+                Sign In
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Button as={Link} color="primary" href="#" variant="flat">
+                Get Started
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </>
+      )}
 
       {/* mobile view */}
       <NavbarMenu>
@@ -84,8 +137,13 @@ export const Navbar = () => {
             type="search"
           />
         </NavbarItem>
-        <NavbarItem className="text-sm">Our Story</NavbarItem>
-        <NavbarItem className="text-sm">Write</NavbarItem>
+        <NavbarItem className="text-medium">Our Story</NavbarItem>
+        <NavbarItem className="text-medium">
+          <div className="flex items-center gap-1 cursor-pointer">
+            <TfiWrite />
+            write
+          </div>
+        </NavbarItem>
       </NavbarMenu>
     </Nav>
   );
