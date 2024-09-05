98fae6210633f0492ff91d93c317ba4b8c1d45ba
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-05 10:57:05 +0600

    basic layout of homepage content

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,19 +1,28 @@
+import { Card, CardHeader } from "@nextui-org/react";
 import React from "react";
 
 export const Homepage = () => {
   return (
-    <div className="container mx-auto grid">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 lg:grid-cols-12 gap-4 px-4 py-6 ">
+      {/* TODO: toggle button for right sidebar for mobile view */}
+
       {/* left sidebar */}
-      <aside>
-        <div>left sidebar content</div>
+      <aside className="hidden md:block md:col-span-1 lg:col-span-3 p-4">
+        <Card>
+          <CardHeader>left sidebar content</CardHeader>
+        </Card>
       </aside>
+
       {/* main content */}
-      <main>
+      <main className="col-span-1 md:col-span-2 lg:col-span-6 p-4 text-center">
         <div>main blog content</div>
       </main>
+
       {/* right sidebar content */}
-      <aside>
-        <div>right sidebar content</div>
+      <aside className=" col-span-1 lg:col-span-3 p-4">
+        <Card>
+          <CardHeader>right sidebar content</CardHeader>
+        </Card>
       </aside>
     </div>
   );
