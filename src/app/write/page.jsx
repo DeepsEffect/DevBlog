aff35d79e6fa9476131b03026ba1403279f18cba
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-18 18:51:33 +0600

    fixed allignment of category section for mobile

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -152,7 +152,7 @@ const WritePage = () => {
           </section>
 
           {/* cover photo and category section section */}
-          <section className="flex gap-4">
+          <section className=" flex flex-col lg:fle-row gap-4">
             {/* cover photo */}
             <div className="w-full">
               <Input
@@ -170,7 +170,6 @@ const WritePage = () => {
                 size="sm"
                 radius="md"
                 variant="flat"
-                isRequired
                 label="Select a category"
                 onChange={(e) => setCategory(e.target.value)}
                 value={category}
