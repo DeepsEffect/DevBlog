37ecfdcbc63d54f96edc121aa83583403de9592a
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-20 21:05:17 +0600

    update connectDB

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,21 +1,28 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-let db;
+let client = null;
+
 export const connectDB = async () => {
-  if (db) return db;
   try {
+    // Close the previous connection if it exists
+    if (client) {
+      await client.close();
+    }
+
     const uri = process.env.MONGODB_URI;
-    const client = new MongoClient(uri, {
+    client = new MongoClient(uri, {
       serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
       },
     });
+
     await client.connect();
-    db = client.db("DevBlog");
+    const db = client.db("DevBlog");
     return db;
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
+    throw error; // Rethrow the error so it can be handled by the caller
   }
 };
