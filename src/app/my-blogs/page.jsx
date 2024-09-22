7ad2d1364614dc25fe01de95e990bc789995f329
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-22 21:07:56 +0600

    added suspense and loading spinner in my-blogs page

--- src/app/my-blogs/page.jsx
+++ src/app/my-blogs/page.jsx
@@ -2,20 +2,32 @@
 
 import BlogCard from "@/components/Homepage/BlogCard/BlogCard";
 import useBlogs from "@/hooks/useBlogs";
+import { Spinner } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const MyBlogsPage = () => {
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
-  const { blogs, loading, error } = useBlogs({ email });
+  const { blogs, loading } = useBlogs({ email });
   // console.log(blogs);
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen gap-2">
+        <Spinner size="sm" />
+        <p>Loading blog...</p>
+      </div>
+    );
+  }
 
   return (
-    <div className="max-w-3xl mx-auto grid gird-cols-1 gap-4 lg:p-4 mt-4 lg:mt-2">
-      {blogs?.map((blog) => (
-        <BlogCard blog={blog} pageType="my-blogs" key={blog._id} />
-      ))}
-    </div>
+    <Suspense>
+      <div className="max-w-3xl mx-auto grid gird-cols-1 gap-4 lg:p-4 mt-4 lg:mt-2">
+        {blogs?.map((blog) => (
+          <BlogCard blog={blog} pageType="my-blogs" key={blog._id} />
+        ))}
+      </div>
+    </Suspense>
   );
 };
 
