7cf4eb223dac7771e19ca0c04ce7affe98f5dc22
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-26 03:51:44 +0600

    made separate components for the sidebard

---
+++ src/components/Homepage/BlogCard/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,115 @@
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import React from "react";
+
+export const LeftSidebar = () => {
+  // category lists
+  const catItems = [
+    {
+      key: "web-dev",
+      label: "Web Dev",
+    },
+    {
+      key: "frontend-dev",
+      label: "Front-end Dev",
+    },
+    {
+      key: "backend-dev",
+      label: "Back-end Dev",
+    },
+    {
+      key: "fullstack-dev",
+      label: "Full-stack Dev",
+    },
+    {
+      key: "framework",
+      label: "Framework",
+    },
+    {
+      key: "javascript",
+      label: "Javascript",
+    },
+    {
+      key: "typescript",
+      label: "TypeScript",
+    },
+    {
+      key: "react",
+      label: "React.js",
+    },
+    {
+      key: "next.js",
+      label: "Next.js",
+    },
+    {
+      key: "angular",
+      label: "Angular",
+    },
+    {
+      key: "vue",
+      label: "Vue.js",
+    },
+    {
+      key: "tailwind",
+      label: "Tailwind CSS",
+    },
+    {
+      key: "node.js",
+      label: "Node.js",
+    },
+    {
+      key: "database",
+      label: "Database",
+    },
+    {
+      key: "devops",
+      label: "DevOps",
+    },
+    {
+      key: "blockchain",
+      label: "Blockchain",
+    },
+    {
+      key: "security",
+      label: "Security",
+    },
+    {
+      key: "cloud",
+      label: "Cloud Computing",
+    },
+    {
+      key: "testing",
+      label: "Testing",
+    },
+    {
+      key: "ai-ml",
+      label: "AI/ML",
+    },
+    {
+      key: "open-source",
+      label: "Open Source",
+    },
+    {
+      key: "others",
+      label: "Others",
+    },
+  ];
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <h2 className="text-xl flex justify-center w-full font-bold">
+            Blog Categories
+          </h2>
+        </CardHeader>
+
+        <CardBody className="flex flex-col gap-3">
+          {catItems?.map((item) => (
+            <Button variant="light" key={item.key}>
+              {item.label}
+            </Button>
+          ))}
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,78 +1,33 @@
 export const revalidate = 0;
-import {
-  Button,
-  ButtonGroup,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-} from "@nextui-org/react";
+import { Button, ButtonGroup } from "@nextui-org/react";
 import BlogCard from "./BlogCard/BlogCard";
+import { RightSidebar } from "./BlogCard/RightSidebar/RightSidebar";
+import { LeftSidebar } from "./BlogCard/LeftSidebar/LeftSidebar";
 
 export const Homepage = async () => {
-  console.log("Fetching data at:", new Date().toISOString());
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/blogs/api?t=${Date.now()}`,
     {
       cache: "no-store",
     }
   );
-  console.log("Fetch completed at:", new Date().toISOString());
   const blogs = await res.json();
-  console.log("Data parsed at:", new Date().toISOString());
   return (
-    <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 lg:grid-cols-12 gap-4 md:px-4 py-6 ">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 lg:grid-cols-12 gap-4 md:px-4 py-4 lg:py-6 ">
       {/* TODO: toggle button for right sidebar for mobile view */}
 
       {/* left sidebar */}
       <aside className="hidden md:block md:col-span-1 lg:col-span-3 lg:p-4">
-        <Card>
-          <CardHeader>
-            <h2 className="text-xl flex justify-center w-full font-bold">
-              Blog Categories
-            </h2>
-          </CardHeader>
-
-          <CardBody>
-            <ul className="text-lg font-semibold space-y-3">
-              <Button variant="light" className="w-full">
-                Most Popular
-              </Button>
-              <Button variant="light" className="w-full">
-                Most Recent
-              </Button>
-              <Button variant="light" className="w-full">
-                Javascript
-              </Button>
-              <Button variant="light" className="w-full">
-                React
-              </Button>
-              <Button variant="light" className="w-full">
-                Next.js
-              </Button>
-              <Button variant="light" className="w-full">
-                Tailwind CSS
-              </Button>
-              <Button variant="light" className="w-full">
-                Fullstack Development
-              </Button>
-              <Button variant="light" className="w-full">
-                Front-end Development
-              </Button>
-            </ul>
-          </CardBody>
-        </Card>
+        <LeftSidebar />
       </aside>
 
       {/* main content */}
       <main className="col-span-1 md:col-span-2 lg:col-span-6 lg:p-4 text-center">
         <ButtonGroup>
-          <Button>Relevant</Button>
           <Button>Latest</Button>
-          <Button>Top</Button>
+          <Button>Relevant</Button>
+          <Button>Pogged</Button>
         </ButtonGroup>
-
-        <div>{Date.now()}</div>
 
         {/* show blog cards */}
         <div className="grid gird-cols-1 gap-4 lg:p-4 mt-4 lg:mt-2">
@@ -90,19 +45,7 @@ export const Homepage = async () => {
 
       {/* right sidebar content */}
       <aside className="col-span-1 lg:col-span-3 lg:p-4">
-        <Card>
-          <CardHeader>
-            <h2 className="text-xl font-bold">Recent Activity</h2>
-          </CardHeader>
-          <CardBody>
-            <p>Recommended topics will be shown here</p>
-            <p>recently saved will be shown here</p>
-          </CardBody>
-          <CardHeader>
-            <p>recent comments will be show here</p>
-          </CardHeader>
-          <CardFooter>other banners will be shown here</CardFooter>
-        </Card>
+        <RightSidebar />
       </aside>
     </div>
   );
