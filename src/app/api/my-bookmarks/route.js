c7ef1312d684344dcc81347c7ffa1ff1a3a6a4ae
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-09-30 03:30:28 +0600

    bookmark get api route

--- src/app/api/my-bookmarks/route.js
+++ src/app/api/my-bookmarks/route.js
@@ -1,20 +1,21 @@
 import { connectDB } from "@/lib/connectDB";
 import { NextResponse } from "next/server";
 
-export const GET = async (request, { params }) => {
+export const GET = async () => {
   try {
     // get the user email
-    const email = params.email;
+    const email = "dipuahmed55@gmail.com";
     // connect to db
     const db = await connectDB();
     // find the bookmarks based on email
     const bookmarks = await db
       .collection("bookmarks")
-      .find({ bookmarkBy: email })
+      .find({ "bookmarkedBy.email": email })
       .toArray();
-    if (!bookmarks) {
-      return NextResponse.json({ message: "no bookmarks available" });
+    if (!bookmarks || bookmarks.length === 0) {
+      return NextResponse.json({ message: "no bookmark available" });
     }
+    return NextResponse.json(bookmarks, { status: 200 });
   } catch (error) {
     console.error("Error occurred:", error.message);
     return NextResponse.json({ message: "server error" }, { status: 500 });
